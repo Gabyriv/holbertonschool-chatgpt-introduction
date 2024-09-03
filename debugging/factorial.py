b4fb1f28cc64796dbcf1810e388e8df58bce14a0
Gabriel <gabyrivm06@gmail.com>
2024-09-03 11:02:27 -0400

    new fix code for the task 0

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -7,20 +7,10 @@ def factorial(n):
     while n > 1:
         result *= n
         n -= 1
-        return result
+    return result
 
-#Check if an argument is provided
-if len(sys.argv) < 2:
-    print("Error: Please provide a number as a command-line argument.")
-    sys.exit(1) #Exit the program with an error code
-
-try:
-    #try to convert the first command-line argument to an integer
-    number = int(sys.argv[1])
-except ValueError:
-    print("Error: Please provide a valid integer.")
-    sys.exit(1) #Exit the program with an error code
-
-#calculate the fatorial and print the result
-f = factorial(number)
-print(f)
+if len(sys.argv) > 1:
+    f = factorial(int(sys.argv[1]))
+    print(f)
+else:
+    print("Please provide a number as an argument.")
